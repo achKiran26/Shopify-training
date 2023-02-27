273a5c7eda20770b436b010186fe8ff02bd64464
achKiran26 <kiran@anattadesign.com>
2023-02-27 18:31:46 +0530

    change variables

--- src/js/bundles/components/header.js
+++ src/js/bundles/components/header.js
@@ -1,28 +1,28 @@
 /* eslint-disable */
 export const header = () => {
-    const header = document.querySelector(".header__hamburger-block");
-    const overlay = document.querySelector(".header__nav-overlay");
-    const openCart = document.querySelector('.js-cartToggle')
-    const sideDrawer = document.querySelector('.header__cart-drawer')
-    const cartClose = document.querySelector('.header__cart-close')
-  
-    // switching classess
-    header.addEventListener('click', (e) => {
-        document.querySelector('.header').classList.toggle('nav-active')
-      })
-    // remove class overlay
-    overlay.addEventListener('click', (e) => {
-        document.querySelector('.header').classList.remove('nav-active')
-      })
+  const navBurgerButton = document.querySelector(".header__hamburger-block");
+  const overlayDiv = document.querySelector(".header__nav-overlay");
+  const openCart = document.querySelector('.js-cartToggle')
+  const sideDrawer = document.querySelector('.header__cart-drawer')
+  const closeCart = document.querySelector('.header__cart-close')
 
-    openCart.addEventListener('click', (e) => {
-      // console.log('click', e.target)
-      sideDrawer.style.width = "30%"
+  // switching classess
+  navBurgerButton.addEventListener('click', (e) => {
+      document.querySelector('.header').classList.toggle('nav-active')
+    })
+  // remove class overlay
+  overlayDiv.addEventListener('click', (e) => {
+      document.querySelector('.header').classList.remove('nav-active')
     })
 
-    cartClose.addEventListener('click', (e) => {
-      sideDrawer.style.width = "0"
-    })
+  openCart.addEventListener('click', (e) => {
+    // console.log('click', e.target)
+    sideDrawer.style.width = "30%"
+  })
+
+  closeCart.addEventListener('click', (e) => {
+    sideDrawer.style.width = "0"
+  })
 }
 
 
