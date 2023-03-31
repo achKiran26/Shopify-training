4b196853c0db7ca889806fae325007f85b172269
achKiran26 <kiran@anattadesign.com>
2023-03-31 19:05:33 +0530

    worked on refactoring and logic fix

--- src/js/bundles/components/domExcercise.js
+++ src/js/bundles/components/domExcercise.js
@@ -1,50 +1,98 @@
 export const domExcercise = () => {
   const value1 = document.querySelector('.js-value1')
   const value2 = document.querySelector('.js-value2')
-  const add = document.querySelector('.js-submit-add')
-  const subtract = document.querySelector('.js-submit-sub')
-  const multiply = document.querySelector('.js-submit-mul')
-  const divide = document.querySelector('.js-submit-div')
-  const result = document.querySelector('.js-result')
-  const container = document.querySelector('.js-op_block')  
+  const addSubmit = document.querySelector('.js-submit-add')
+  const subSubmit = document.querySelector('.js-submit-sub')
+  const mulSubmit = document.querySelector('.js-submit-mul')
+  const divSubmit = document.querySelector('.js-submit-div')
+  const ranSubmit = document.querySelector('.js-submit-ran')
+  const addResult = document.querySelector('.js-result-add')
+  const subResult = document.querySelector('.js-result-sub')
+  const mulResult = document.querySelector('.js-result-mul')
+  const divResult = document.querySelector('.js-result-div')
+  const ranResult = document.querySelector('.js-result-ran')
+  const input = document.querySelector('.inputResult')
+  const error = document.querySelector('.js-error')
+  const container = document.querySelector('.js-op_block')
 
-  const arithmaticOp = [add,subtract,multiply,divide]
-  //exe
-  arithmaticOp.forEach(e => {
-    e.addEventListener('click',()=>{
-    const msg = "Please add the input value"
-
-    if(value1.value == '' || value2.value == ''){
-      container.textContent = msg
+  function errHandle(){
+    const errorEmptyFields = "Please add the input value"
+    const errorEmptyField1 = "Please add the input value for 1st input field"
+    const errorEmptyField2 = "Please add the input value for 2nd input field"
+    if(value1.value == ''){
+      error.textContent = errorEmptyField1
+      container.classList.remove('success')
+      container.classList.add('error')
+    }else if(value2.value == ''){
+      error.textContent = errorEmptyField2
       container.classList.remove('success')
       container.classList.add('error')
     }
-    else if(value1.value && value2.value){
-      const output = [addition(value1,value2), sub(value1,value2), mul(value1,value2), div(value1,value2)];
-      result.innerHTML = output
+    else if(value1.value == '' || value2.value == ''){
+      error.textContent = errorEmptyFields
+      container.classList.remove('success')
+      container.classList.add('error')
+      input.textContent = " "
+      
+    }else if(value1.value && value2.value){
+      addSubmit.remove()
       container.classList.remove('error')
       container.classList.add('success')
     }
-    else{
-      
-    }
+  }
+
+  // exe
+  addSubmit.addEventListener("click", () => {
+    errHandle()
+    let result = add(value1.value,value2.value);
+    console.log(result)
+    addResult.innerHTML = result
   })
-})
-// logic for add,sub,mul,div
-  function addition(){
-    return parseInt(value1.value )+parseInt(value2.value)
+
+  subSubmit.addEventListener("click", () => {
+    errHandle()
+    let result = subtract(value1.value, value2.value)
+    subResult.innerHTML = result;subResult
+  })
+
+  mulSubmit.addEventListener("click", () => {
+    errHandle()
+    let result = multiply(value1.value, value2.value)
+    mulResult.innerHTML = result
+  })
+
+  divSubmit.addEventListener("click", () => {
+    errHandle()
+    let result = divide(value1.value, value2.value)
+    divResult.innerHTML = result
+  })
+
+  ranSubmit.addEventListener("click", () => {
+    errHandle()
+    let result = random(value1.value, value2.value)
+    console.log(result)
+    ranResult.innerHTML = result
+  })
+
+  // Logic
+  const add = (value1,value2) => {
+    return parseInt(value1)+parseInt(value2)
   }
 
-  function sub(){
-    return parseInt(value1.value)-parseInt(value2.value)
+  const subtract = (value1,value2) => {
+    return parseInt(value1)-parseInt(value2)
   }
 
-  function mul(){
-    return parseInt(value1.value)*parseInt(value2.value)
+  const multiply = (value1,value2) => {
+    return parseInt(value1)*parseInt(value2)
   }
 
-  function div(){
-    return parseInt(value1.value)/parseInt(value2.value)
+  const divide = (value1,value2) => {
+    return parseInt(value1)/parseInt(value2)
+  }
+
+  const random = (value1,value2) => {
+    return Math.floor(Math.random() * value1 + value2) + 1;
   }
 }
 domExcercise()
