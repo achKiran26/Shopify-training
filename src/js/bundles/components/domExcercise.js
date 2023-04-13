a5c5e63c0c021aed08c29a3038a27bfe152c70d6
achKiran26 <kiran@anattadesign.com>
2023-04-13 18:35:07 +0530

    refactor the repeatable code

--- src/js/bundles/components/domExcercise.js
+++ src/js/bundles/components/domExcercise.js
@@ -1,15 +1,22 @@
 
 export const domExcercise = () => {
+  const globalDeclarations={
+     value1: 'js-value1',
+     value2: 'js-value2',
+     error: 'js-error',
+     sumit: 'js-submit',
+     finalResult: 'js-result',
+  }
   const calculationFunctions = [
     //add
     {
       name: 'add',
       container: 'js-op_block-add',
-      error: 'js-error-add',
-      sumit: 'js-submit-add',
-      value1: 'js-addValue1',
-      value2: 'js-addValue2',
-      finalResult: 'js-result-add',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun(value1, value2){
         return parseInt(value1) + parseInt(value2)
       }
@@ -18,11 +25,11 @@ export const domExcercise = () => {
     {
       name: 'sub',
       container: 'js-op_block-sub',
-      error: 'js-error-sub',
-      sumit: 'js-submit-sub',
-      value1: 'js-subValue1',
-      value2: 'js-subValue2',
-      finalResult: 'js-result-sub',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) {
         return parseInt(value1) - parseInt(value2)
       }
@@ -31,11 +38,11 @@ export const domExcercise = () => {
     {
       name: 'mul',
       container: 'js-op_block-mul',
-      error: 'js-error-mul',
-      sumit: 'js-submit-mul',
-      value1: 'js-mulValue1',
-      value2: 'js-mulValue2',
-      finalResult: 'js-result-mul',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2){ 
         return parseInt(value1) * parseInt(value2)
       }
@@ -44,11 +51,11 @@ export const domExcercise = () => {
     {
       name: 'div',
       container: 'js-op_block-div',
-      error: 'js-error-div',
-      sumit: 'js-submit-div',
-      value1: 'js-divValue1',
-      value2: 'js-divValue2',
-      finalResult: 'js-result-div',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) { 
         return value1 / value2
       }
@@ -57,80 +64,76 @@ export const domExcercise = () => {
     {
       name: 'ran',
       container: 'js-op_block-ran',
-      error: 'js-error-ran',
-      sumit: 'js-submit-ran',
-      value1: 'js-ranValue1',
-      value2: 'js-ranValue2',
-      finalResult: 'js-result-ran',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) { 
         if (value1 > value2) {
           const error = document.querySelector('.js-error-ran') 
-          error.innerHTML="Not a valid entry"
+          error.innerHTML="Not valid"
           value1=""
           value2=""
-          container.classList.remove('success')
-          container.classList.add('error')
+          errorState(container)
         }
-        const result = Math.floor(Math.random() * (value2 - value1 + 1) + value1)
-        // while(result === 0){
-        //   Math.floor(Math.random() * (value2 - value1 + 1) + value1)
-        // }
-        return result
+        return Math.floor(Math.random() * (value2 - value1 + 1) + value1)
       }
     },
     //count str
     {
-      name: 'count str',
+      name: 'str',
       container: 'js-op_block-str',
-      error: 'js-error-str',
-      sumit: 'js-submit-str',
-      value1: 'js-strValue1',
-      finalResult: 'js-result-str',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1) { 
         return value1.length
     }
     },
     //concat
     {
-      name: 'concat',
+      name: 'con',
       container: 'js-op_block-con',
-      error: 'js-error-con',
-      sumit: 'js-submit-con',
-      value1: 'js-conValue1',
-      value2: 'js-conValue2',
-      finalResult: 'js-result-con',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) { 
         return value1.concat(value2)
     }
     },
     //check of str
     {
-      name: 'repeat',
+      name: 'rep',
       container: 'js-op_block-rep',
-      error: 'js-error-rep',
-      sumit: 'js-submit-rep',
-      value1: 'js-repValue1',
-      value2: 'js-repValue2',
-      finalResult: 'js-result-rep',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) { 
         if (value1.includes(value2)) {
           return true
         }else{
-          error.classList.remove("success")
-          error.classList.add("error")
+          // container.classList.remove("success")
+          // container.classList.add("error")
           return false
         } 
       }
     },
     //check for str count
     {
-      name: 'rep str count',
+      name: 'repC',
       container: 'js-op_block-repC',
-      error: 'js-error-repC',
-      sumit: 'js-submit-repC',
-      value1: 'js-repCValue1',
-      value2: 'js-repCValue2',
-      finalResult: 'js-result-repC',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1, value2) { 
         const reg = new RegExp(value2,"g")
           const count = (value1.match(reg)|| []).length
@@ -139,12 +142,13 @@ export const domExcercise = () => {
     },
     //reverse
     {
-      name: 'reverse',
+      name: 'rev',
       container: 'js-op_block-revStr',
-      error: 'js-error-revStr',
-      sumit: 'js-submit-revStr',
-      value1: 'js-revStrValue1',
-      finalResult: 'js-result-revStr',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1) { 
         const arr = value1.split("")
         const rev = arr.reverse()
@@ -154,12 +158,13 @@ export const domExcercise = () => {
     },
     //email validation
     {
-      name: 'email',
+      name: 'em',
       container: 'js-op_block-em',
-      error: 'js-error-em',
-      sumit: 'js-submit-em',
-      value1: 'js-emValue1',
-      finalResult: 'js-result-em',
+      error: globalDeclarations.error,
+      sumit: globalDeclarations.sumit,
+      value1:globalDeclarations.value1,
+      value2:globalDeclarations.value2,
+      finalResult: globalDeclarations.finalResult,
       outputFun (value1) { 
         const regValue = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
         function errorState() {
@@ -178,12 +183,12 @@ export const domExcercise = () => {
     },
     //input fields validation
     {
-      name: 'str check',
+      name: 'strCheck',
       container: 'js-op_block-strCheck',
-      error: 'js-error-strCheck',
-      sumit: 'js-submit-strCheck',
-      value1: 'js-strCheckValue1',
-      finalResult: 'js-result-strCheck',
+      error: 'js-error',
+      sumit: 'js-submit',
+      value1: 'js-value1',
+      finalResult: 'js-result',
       outputFun (value1) { 
         const errorCheck = document.querySelector(".js-result-strCheckerror")
         const errorArr =[]
@@ -217,17 +222,18 @@ export const domExcercise = () => {
   ]
   //creating looping to target each modules
   calculationFunctions.forEach(res => {
-    const submit = document.querySelector(`.${res.sumit}`)
-    const value1 = document.querySelector(`.${res.value1}`)
-    const value2 = document.querySelector(`.${res.value2}`)
-    const result = document.querySelector(`.${res.finalResult}`)
-    const error = document.querySelector(`.${res.error}`)
     const container = document.querySelector(`.${res.container}`)
+    const parent = document.getElementById(`${res.name}`)
+    const submit = parent.querySelector(`.${res.sumit}`)
+    const value1 = parent.querySelector(`.${res.value1}`)
+    const value2 = parent.querySelector(`.${res.value2}`)
+    const result = parent.querySelector(`.${res.finalResult}`)
+    const error = parent.querySelector(`.${res.error}`)
+    
     submit.addEventListener("click", () => {
-
       if (value1 && value2) {
-        const num1 = value1.value || 0
-        const num2 = value2.value || 0
+        const num1 = value1.value || 0 
+        const num2 = value2.value || 0 
         if (errorHandleInputs(num1,num2,error,container,result)) {
           return
         }
@@ -264,7 +270,7 @@ export const domExcercise = () => {
     if (num1 == "" && num2 == "") {
       error.innerHTML = errorEmptyFields
       res.classList.remove('block')
-      res.classList.toggle('none')
+      res.classList.add('none')
       errorState(container)
       return true
     }
@@ -280,18 +286,18 @@ export const domExcercise = () => {
   }
   function errorHandleInput(num1, error, container, res) {
     const errorEmptyField2 = "Input field cannot be empty"
-        if(num1 == ""){
-        error.textContent = errorEmptyField2
-        errorState(container)
-        hideBlock(res)
-        return true
-      }
-      error.innerHTML="Result"
-      showBlock(res)
-      successState(container)
-      return false
+    if(num1 == ""){
+      error.textContent = errorEmptyField2
+      errorState(container)
+      hideBlock(res)
+      return true
     }
+    error.innerHTML="Result"
+    showBlock(res)
+    successState(container)
+    return false
   }
+}
 domExcercise()
   
   
